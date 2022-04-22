acaa20b0f5dc782c9b954e62b864287dfc038011
Sergey <kurilovsergey15@gmail.com>
2022-04-22 14:15:18 +0300

    #114 Course hero

---
+++ pages/courses/[slug].js
@@ -0,0 +1,44 @@
+import {CourseHero, Curriculum, KeyPoints} from "@components/course";
+import {Modal} from "@components/common";
+import {BaseLayout} from "@components/layout";
+import {getAllCourses} from "@content/courses/fetcher";
+
+export default function Course({course}) {
+
+    return (
+        <>
+            <div className="py-4">
+                <CourseHero
+                    title={course.title}
+                    description={course.description}
+                    image={course.coverImage}
+                />
+            </div>
+            <KeyPoints />
+            <Curriculum />
+            <Modal />
+        </>
+    )
+}
+
+export function getStaticPaths() {
+    const {data} = getAllCourses();
+    return {
+        paths: data.map(c => ({params: {slug: c.slug}})),
+        fallback: false
+    }
+}
+
+export function getStaticProps({params}) {
+    const {data} = getAllCourses()
+
+    const course = data.filter(c => c.slug === params.slug)[0]
+
+    return {
+        props: {
+            course
+        }
+    }
+}
+
+Course.Layout = BaseLayout
